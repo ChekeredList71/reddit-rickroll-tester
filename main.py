855d5d75a0cedc8ba9083c27d37d6460e30a5662
Peter Nasz <naszpeti@gmail.com>
2024-04-16 13:42:46 +0200

    example in main.py
    made this for programming partner

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import ricklib
 
+if ricklib.is_link_valid(""):
+    comment = ricklib.extract_comment("")
+
+    if ricklib.is_text_ricked(comment):
+        print("van")
+    else:
+        print("nincs")
