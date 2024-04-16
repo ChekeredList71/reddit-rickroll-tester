f50d9c7d8fe9bde459f1b76b405c7bbab98e4c0c
Peter Nasz <naszpeti@gmail.com>
2024-04-16 13:17:23 +0200

    fixes
    made linkchecker usable for user post comments and fixed extract_comment

--- ricklib.py
+++ ricklib.py
@@ -5,6 +5,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 import re
 
+# load rickroll links
+ricklinks = [i.strip() for i in open("ricklinks.txt")]
+
 
 def extract_comment(url: str):
     # set up headless mode (so the Google Chrome window doesn't open)
@@ -19,14 +22,21 @@ def extract_comment(url: str):
 
     # when in headless mode, driver.find_element will fail, because the website doesn't load the comment when immediately
     # we need to wait for the browser to take the JavaScript AJAX request, and then scrape it
-    waiter = WebDriverWait(driver, 3)
-    waiter.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#t1_kscnr2t-comment-rtjson-content")))
+    waiter = WebDriverWait(driver, 6)
+    waiter.until(EC.visibility_of_element_located((By.XPATH, "/html/body/shreddit-app/dsa-transparency-modal-provider/div/div[1]/div/main/shreddit-comment-tree/shreddit-comment/div[3]/div/p")))
 
-    text = driver.find_element(By.CSS_SELECTOR, "#t1_kscnr2t-comment-rtjson-content > div:nth-child(1) > p:nth-child(1)").text
+    text = driver.find_element(By.XPATH, "/html/body/shreddit-app/dsa-transparency-modal-provider/div/div[1]/div/main/shreddit-comment-tree/shreddit-comment/div[3]/div/p").text
     driver.quit()
     return text
 
 
 def is_link_valid(url: str):
-    return re.match(r"https://www.reddit.com/r/[0-z]+/comments/[0-z]+/comment/[0-z]+/\?", url) is not None
+    return re.match(r"https://www.reddit.com/[0-z]+/[0-z]+/comments/[0-z]+/comment/[0-z]+/\?", url) is not None
 
+
+def is_text_ricked(text: str):
+    # get link with regex
+    pass
+
+
+print(extract_comment("https://www.reddit.com/r/legaladvice/comments/1asv5yz/comment/kqt1dik/"))
